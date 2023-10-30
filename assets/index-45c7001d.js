d7965fde85f5aa2067979700dd8d3c64247d9c8d
Aoki <ger.vladislav2001@gmail.com>
2023-10-30 23:17:33 +0200

    Updates

--- assets/index-45c7001d.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index-2ec5438e.js";const s=()=>e.jsx("div",{children:"Here will be AnotherNews"});export{s as default};
---
+++ assets/index-4f995ae0.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-72a03fd1.js";const r=()=>e.jsx("div",{children:"Here will be News"});export{r as default};
