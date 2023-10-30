4b126af8a376802e34fc1886b56a998860c04fd6
Aoki <ger.vladislav2001@gmail.com>
2023-10-30 19:26:39 +0200

    Updates

---
+++ assets/index-0ca1dada.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-197ec5df.js";const s=()=>e.jsx("div",{children:"Here will be AnotherNews"});export{s as default};
---
+++ assets/index-7443b892.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-197ec5df.js";const r=()=>e.jsx("div",{children:"Here will be Memes"});export{r as default};
